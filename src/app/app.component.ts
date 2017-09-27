988160dcc850ee28c655583bab66f1ef0086ac5d
NelsonGomesNeto <ngn@ic.ufal.br>
2017-09-27 17:31:18 -0300

    Merge branch 'master' of https://github.com/The-Dank-Network/TDN-Web
    # Conflicts:
    #	src/app/app.component.html
    Done configurations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { User } from '../model/user.model';
+import {HttpClient, HttpClientModule, HttpHeaders, HttpRequest} from '@angular/common/http';
+import {Headers, RequestOptions} from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -11,12 +13,20 @@ export class AppComponent {
 
   user: User = new User();
 
-  constructor () {
-
+  constructor (private http: HttpClient) {
   }
 
   sendUser() {
+    // headers.append;
+    // let options = new RequestOptions({headers: headers});
+    const header = new HttpHeaders({'Authorization' : 'Bearer 0398fb67-7dff-4616-a446-a697921a18c4'});
+    console.log(header);
 
+    this.http
+      .get('http://localhost:8080/profile', {headers: header})
+      .subscribe(data => {
+        console.log(data);
+      });
     console.log(this.user);
   }
 }
