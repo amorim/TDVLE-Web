60c1e1e3ad961c6109cc88d6ae65b7a4fe953e19
Lucas Amorim <loa@ic.ufal.br>
2019-03-21 16:29:57 -0300

    config

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -1,4 +1,6 @@
 export class Constants {
+  //public static url = 'https://the-dank-network.herokuapp.com/api';
+  // dev
   public static url = 'http://localhost:8080/api';
   public static notificationUpdateTime = 30000;
 }
